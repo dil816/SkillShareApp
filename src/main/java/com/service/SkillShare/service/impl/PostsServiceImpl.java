152721bf855a355b11e640daf5e3cc42345aeae6
dil816 <Rivindudilochana18329@gmail.com>
2025-04-30 00:32:04 +0530

    getallpost service added

--- src/main/java/com/service/SkillShare/service/impl/PostsServiceImpl.java
+++ src/main/java/com/service/SkillShare/service/impl/PostsServiceImpl.java
@@ -1,12 +1,15 @@
 package com.service.SkillShare.service.impl;
 
 import com.service.SkillShare.dto.CreatePostDto;
+import com.service.SkillShare.dto.GetPostDto;
 import com.service.SkillShare.entity.Posts;
 import com.service.SkillShare.mappings.PostMapping;
 import com.service.SkillShare.repository.PostsRepository;
 import com.service.SkillShare.service.PostsService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 
@@ -26,7 +29,41 @@ public class PostsServiceImpl implements PostsService {
     }
 
     @Override
-    public List<Posts> getAllPosts() {
-        return List.of();
+    public List<GetPostDto> getAllPosts() {
+        List<Posts> posts = postsRepository.findAll();
+        if (posts.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+//        GetPostDto data = posts.forEach(post -> {
+//            GetPostDto postDto = new GetPostDto();
+//            postDto.setId(post.getId());
+//            postDto.setTags(post.getTags());
+//            postDto.setContentTitle(post.getContentTitle());
+//            postDto.setPostDescription(post.getPostDescription());
+//            postDto.setImageBase64(
+//                    Base64.getEncoder().encodeToString(
+//                            post.getPostImage().getData()
+//                    )
+//            );
+//        });
+
+        List<GetPostDto> data = posts.stream()
+                .map(post -> {
+                    GetPostDto postDto = new GetPostDto();
+                    postDto.setId(post.getId());
+                    postDto.setTags(post.getTags());
+                    postDto.setContentTitle(post.getContentTitle());
+                    postDto.setPostDescription(post.getPostDescription());
+                    postDto.setImageBase64(
+                            Base64.getEncoder().encodeToString(
+                                    post.getPostImage().getData()
+                            )
+                    );
+                    return postDto;
+                })
+                .toList();
+
+        return data;
     }
 }
