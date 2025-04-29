3b32abd3bb98dfbdbf13327d72b4a1c65223bdb8
dil816 <Rivindudilochana18329@gmail.com>
2025-04-29 18:16:43 +0530

    service changes added

--- src/main/java/com/service/SkillShare/service/PostsService.java
+++ src/main/java/com/service/SkillShare/service/PostsService.java
@@ -1,9 +1,15 @@
 package com.service.SkillShare.service;
 
+import com.service.SkillShare.dto.PostDto;
 import com.service.SkillShare.entity.Posts;
+
+import java.io.IOException;
 import java.util.List;
 
 public interface PostsService {
-    Posts createPost(Posts post);
+    // Create a new Post
+    Posts createPost(PostDto postDto);
+
+    // Get All Posts
     List<Posts> getAllPosts();
 }
