e89dda6892ac4ccd7d34b9e13ab24ad44886a302
dil816 <Rivindudilochana18329@gmail.com>
2025-04-29 12:17:09 +0530

    createpost getallposts added

--- src/main/java/com/service/SkillShare/service/PostsService.java
+++ src/main/java/com/service/SkillShare/service/PostsService.java
@@ -1,4 +1,9 @@
 package com.service.SkillShare.service;
 
+import com.service.SkillShare.entity.Posts;
+import java.util.List;
+
 public interface PostsService {
+    Posts createPost(Posts post);
+    List<Posts> getAllPosts();
 }
