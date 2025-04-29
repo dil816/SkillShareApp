108db1294b5f2525b2bd7c75842718bcd3abd05d
dil816 <Rivindudilochana18329@gmail.com>
2025-04-29 19:22:56 +0530

    GetPostDto Attr added

--- src/main/java/com/service/SkillShare/dto/GetPostDto.java
+++ src/main/java/com/service/SkillShare/dto/GetPostDto.java
@@ -1,4 +1,11 @@
 package com.service.SkillShare.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class GetPostDto {
+    private String id;
+    private String Tags;
+    private String ContentTitle;
+    private String PostDescription;
+    private MultipartFile ImageBase64;
 }
