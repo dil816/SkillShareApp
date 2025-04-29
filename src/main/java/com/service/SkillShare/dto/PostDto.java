f76fc7afb3699d0628eacf237e50e2dc3117bbcd
dil816 <Rivindudilochana18329@gmail.com>
2025-04-29 18:13:55 +0530

    postDto added

---
+++ src/main/java/com/service/SkillShare/dto/PostDto.java
@@ -0,0 +1,15 @@
+package com.service.SkillShare.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class PostDto {
+    private String id;
+    private String Tags;
+    private String ContentTitle;
+    private String PostDescription;
+    private MultipartFile File;  // photo or video
+}
+
+
