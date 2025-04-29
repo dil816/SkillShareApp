d506adec77ba5d205c09b26bbc22fca189141b82
dil816 <Rivindudilochana18329@gmail.com>
2025-04-29 18:17:03 +0530

    service impl added

--- src/main/java/com/service/SkillShare/service/impl/PostsServiceImpl.java
+++ src/main/java/com/service/SkillShare/service/impl/PostsServiceImpl.java
@@ -1,4 +1,33 @@
 package com.service.SkillShare.service.impl;
 
-public class PostsServiceImpl {
+import com.service.SkillShare.dto.PostDto;
+import com.service.SkillShare.entity.Posts;
+import com.service.SkillShare.mappings.PostMapping;
+import com.service.SkillShare.repository.PostsRepository;
+import com.service.SkillShare.service.PostsService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Service
+public class PostsServiceImpl implements PostsService {
+
+    private final PostsRepository postsRepository;
+
+    PostsServiceImpl(PostsRepository postsRepository) {
+        this.postsRepository = postsRepository;
+    }
+
+    @Override
+    public Posts createPost(PostDto postDto) {
+        Posts posts = PostMapping.ToEntity(postDto);
+        return postsRepository.insert(posts);
+    }
+
+    @Override
+    public List<Posts> getAllPosts() {
+        return List.of();
+    }
 }
