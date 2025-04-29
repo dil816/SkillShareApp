67c4cc2f14151698232ba8e391d5f87f9f2761c2
dil816 <Rivindudilochana18329@gmail.com>
2025-04-29 12:04:33 +0530

    post entity Attributes added

--- src/main/java/com/service/SkillShare/entity/Posts.java
+++ src/main/java/com/service/SkillShare/entity/Posts.java
@@ -1,5 +1,29 @@
 package com.service.SkillShare.entity;
 
-public class Posts {
+import lombok.*;
+import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.InputStream;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "posts")
+public class Posts {
+    @Id
+    private String id;
+    private String tags;
+    private String ContentTitle;
+    private Binary PostImage;
+    private InputStream PostVideo;
+    private String PostDescription;
+    private LocalDateTime PublishedAt;
+
+    public  void  setPublishedAt() {
+        this.PublishedAt = LocalDateTime.now();
+    }
 }
