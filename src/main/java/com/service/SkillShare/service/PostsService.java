82d450e1b12dbc9c1837499e74085e3caf68d032
dil816 <Rivindudilochana18329@gmail.com>
2025-04-30 11:58:09 +0530

    tempory exception handeling added

--- src/main/java/com/service/SkillShare/service/PostsService.java
+++ src/main/java/com/service/SkillShare/service/PostsService.java
@@ -4,11 +4,12 @@ import com.service.SkillShare.dto.CreatePostDto;
 import com.service.SkillShare.dto.GetPostDto;
 import com.service.SkillShare.entity.Posts;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface PostsService {
     // Create a new Post
-    Posts createPost(CreatePostDto createPostDto);
+    Posts createPost(CreatePostDto createPostDto) throws IOException;
 
     // Get All Posts
     List<GetPostDto> getAllPosts();
