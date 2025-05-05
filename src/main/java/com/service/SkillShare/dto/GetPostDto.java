07b0a1b86ad44f6891a2dbf125aa67a23453a859
dil816 <Rivindudilochana18329@gmail.com>
2025-05-05 13:06:39 +0530

    unuse import removed

--- src/main/java/com/service/SkillShare/dto/GetPostDto.java
+++ src/main/java/com/service/SkillShare/dto/GetPostDto.java
@@ -1,6 +1,5 @@
 package com.service.SkillShare.dto;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +10,6 @@ public class GetPostDto {
     private String Tags;
     private String ContentTitle;
     private String PostDescription;
-    private String ImageBase64;
-
     private String ImageUrl;
     private String VideoUrl;
-
-
 }
