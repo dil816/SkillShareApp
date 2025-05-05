cf4160420a491327c3c82df2155842a19f13eed5
dil816 <Rivindudilochana18329@gmail.com>
2025-05-05 13:09:37 +0530

    unuse import removed in entity

--- src/main/java/com/service/SkillShare/entity/Posts.java
+++ src/main/java/com/service/SkillShare/entity/Posts.java
@@ -1,7 +1,6 @@
 package com.service.SkillShare.entity;
 
 import lombok.*;
-import org.bson.types.Binary;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
