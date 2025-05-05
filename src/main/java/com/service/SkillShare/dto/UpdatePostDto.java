80bd3e23d872e4c456dbed9d8da6476a56524af8
dil816 <Rivindudilochana18329@gmail.com>
2025-05-05 10:11:54 +0530

    update dto added

---
+++ src/main/java/com/service/SkillShare/dto/UpdatePostDto.java
@@ -0,0 +1,13 @@
+package com.service.SkillShare.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class UpdatePostDto {
+    private String id;
+    private String Tags;
+    private String ContentTitle;
+    private String PostDescription;
+    private MultipartFile ImgFile;  // photo or video
+}
