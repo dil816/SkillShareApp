657c840dbfe4333ea2a35e33cb5dc9be876936e8
dil816 <Rivindudilochana18329@gmail.com>
2025-05-06 10:51:07 +0530

    structure refactor

--- Services/src/main/java/com/service/SkillShare/dto/post/CreatePostDto.java
+++ Services/src/main/java/com/service/SkillShare/dto/post/CreatePostDto.java
@@ -1,13 +1,15 @@
-package com.service.SkillShare.dto;
+package com.service.SkillShare.dto.post;
 
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
-public class UpdatePostDto {
+public class CreatePostDto {
     private String id;
     private String Tags;
     private String ContentTitle;
     private String PostDescription;
     private MultipartFile MediaFile;  // photo or video
 }
+
+
