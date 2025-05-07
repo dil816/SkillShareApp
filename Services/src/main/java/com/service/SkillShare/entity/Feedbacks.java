b9c47278f9b85a98f6c35eb47c17fe9f5c7f42b7
Kavindu49 <158990749+Kavindu49@users.noreply.github.com>
2025-05-07 16:29:03 +0530

    feedback entity added

--- Services/src/main/java/com/service/SkillShare/entity/Feedbacks.java
+++ Services/src/main/java/com/service/SkillShare/entity/Feedbacks.java
@@ -1,4 +1,27 @@
 package com.service.SkillShare.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "feedbacks")
+
 public class Feedbacks {
+
+@Id
+private String id;
+private String Comment;
+private String Author;
+private String AreatedAt;
+private String LikeCount;
+
+
 }
