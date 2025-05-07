c651b3112e582e2d8c896ddd22f4b9ac6bfd2f9b
Kavindu49 <158990749+Kavindu49@users.noreply.github.com>
2025-05-07 21:12:58 +0530

    feedback service changes

--- Services/src/main/java/com/service/SkillShare/entity/Feedbacks.java
+++ Services/src/main/java/com/service/SkillShare/entity/Feedbacks.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -20,7 +22,7 @@ public class Feedbacks {
 private String id;
 private String Comment;
 private String Author;
-private String CreatedAt;
+private Date CreatedAt = new Date();
 private String LikeCount;
 
 
