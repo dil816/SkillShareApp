7e02fb2b2199fe1a7e3df792c88cb15c75972576
osura <john.doe@contoso.com>
2025-05-10 02:14:43 +0530

    header changes

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -27,14 +27,27 @@ function Header() {
                     <div className="main-menu">
                       <nav>
                         <ul id="navigation">
-                          <li><Link to="/user/home">Home</Link></li> {/* Use Link for navigation */}
-                          <li><Link to="/user/about">About</Link></li> {/* Use Link for navigation */}
-                          <li><Link to="/user/skill-post-view">Category</Link></li>
-                          <li><Link to="/user/blog-details">Latest News</Link></li>
-                                                    <li><Link to="/user/event-workshop-all">Events & Workshops</Link></li>
-                          
-                          <li><Link to="#">Pages</Link>
-                          <li><Link to="/user/learning-plans">Learning Plans</Link></li>
+                          <li>
+                            <Link to="/user/home">Home</Link>
+                          </li>{" "}
+                          {/* Use Link for navigation */}
+                          <li>
+                            <Link to="/user/about">About</Link>
+                          </li>{" "}
+                          {/* Use Link for navigation */}
+                          <li>
+                            <Link to="/user/skill-post-view">Post</Link>
+                          </li>
+                          <li>
+                            <Link to="/user/blog-details">Latest News</Link>
+                          </li>
+                          <li>
+                            <Link to="/user/event-workshop-all">
+                              Events & Workshops
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="#">Pages</Link>
                             <ul className="submenu">
                               <li>
                                 <Link to="/user/blog">Blog</Link>
