11cc20c2010ba0d615b86ec242f87e80f545ebdf
osura <john.doe@contoso.com>
2025-05-10 02:11:06 +0530

    ui changes

--- Client/src/components/UserProfile/UserCard.jsx
+++ Client/src/components/UserProfile/UserCard.jsx
@@ -8,9 +8,9 @@ function UserCard() {
                   <div className="d-flex flex-column align-items-center text-center">
                     <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
                     <div className="mt-3">
-                      <h4>John Doe</h4>
+                      <h4>Osura Samoda</h4>
                       <p className="text-secondary mb-1">Full Stack Developer</p>
-                      <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                      <p className="text-muted font-size-sm">Townhall</p>
                       <p className="text-muted font-size-sm">Folowers :  320</p>
                       <p className="text-muted font-size-sm">Folowing :  220</p>
                       <button className="btn btn-primary">Follow</button>
